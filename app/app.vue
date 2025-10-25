8f516430eb8b8a8d11c8cee359a6c9a0a690442c
Dima.KItsenko <dima.kitsenko2001@gmail.com>
2025-10-25 20:34:51 +0000

    all add

---
+++ app/app.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="w-full h-28 bg-black flex items-center justify-around">
+    <div class="flex items-center">
+      <a href="#"><img class="w-[53px] h-[54px]" src="/Logo.png" alt="logo"></a>
+      <a href="#" class="text-white flex ml-[47px]">Jogos ∇</a>
+      <a href="#" class="text-white ml-[24px]">Loja</a>
+      <a href="#" class="text-white ml-[24px]">Ajuda</a>
+    </div>
+    <div class="flex">
+      <button class="ml-[522px] w-[193px] h-[52px] font-medium rounded-md bg-[#FCAF17]">Baixe o Launcher</button>
+      <button class="ml-[40px] w-[48px] h-[48px] bg-[#5A5A5A] rounded-full flex items-center justify-center"><img src="/search.png" alt="search"></button>
+      <button class="ml-[24px] w-[48px] h-[48px] bg-[#5A5A5A] rounded-full flex items-center justify-center"><img src="/person.png" alt="person"></button>
+    </div>
+  </div>
+  <img class="w-full h-auto" src="/GTA-6-fon.png" alt="GTA-6-fon">
+  <div class="w-full h-[747px] bg-[#101010] flex justify-center">
+    <div class="w-[1160px] h-[555px] mt-[80px] flex items-center flex-col">
+      <div class="flex">
+        <h1 class="not-italic font-extrabold text-[48px] text-white">Jogos em Destaque</h1>
+        <div class="ml-[296px] flex items-center">
+          <button class="text-white h-[32px] bg-[#606060] rounded-full px-6">PC</button>
+          <button class="text-white h-[32px] bg-[#606060] rounded-full px-6 ml-[16px]">Console</button>
+          <button class="text-white h-[32px] bg-[#606060] rounded-full px-6 ml-[16px]">Dispositivos Portáteis</button>
+        </div>
+      </div>
+      <div class="w-full flex justify-between scroll-px-[24px] mt-[40px]">
+        <a href="#"><img src="/GTA-5.png" alt="GTA-5"></a>
+        <a href="#"><img src="/GTA-online.png" alt="GTA-5"></a>
+        <a href="#"><img src="/RED.png" alt="GTA-5"></a>
+        <a href="#"><img src="/RED-Online.png" alt="GTA-5"></a>
+      </div>
+      <button class= "w-[193px] h-[52px] font-medium rounded-md mt-[64px] bg-[#FCAF17]">Ver todos os jogos</button>
+    </div>
+  </div>
+  <div class="w-full h-[712px] bg-[#101010] flex justify-center">
+    <div class="w-[1160px] h-[555px] flex flex-col">
+      
+      <h1 class="not-italic font-extrabold text-[48px] text-white">Jogos em Destaque</h1>
+      
+      <div class="flex">
+        <div class="w-[860px] h-[488px] FCAF17 bg-black flex flex-col scroll-px-[24px] mt-[40px]">
+          <img class="rounded-t-lg" src="/GTA-6.png" alt="GTA-5">
+          <h1 class="not-italic font-medium text-[32px] text-white mt-[24px] ml-[32px]">Grand Theft Auto VI - Veja o 1º trailer agora</h1>
+          <div class="flex">
+            <p class="text-[#797979] mt-[8px] ml-[32px]">Rockstar Games</p>
+            <p class="ml-[25px] text-[#797979] mt-[8px]">5 de dezembro de 2023</p>
+          </div>
+        </div>
+        <div class="w-[300px] h-[488px] flex flex-col justify-between items-end mt-auto">
+          <div class="w-[260px] h-[224px] rounded-lg bg-black flex flex-col">
+            <img class="rounded-t-lg" src="/foto-1.png" alt="foto">
+            <h1 class="w-[191px] h-[40px] not-italic font-medium text-[13px] text-white mt-[4px] ml-[12px]">1º Trailer. Terça-feira, 5 de Dezembro. 9h Et.</h1>
+            <div class="flex">
+              <p class="text-[#797979] text-[12px] mt-[8px] ml-[12px]">Rockstar Games</p>
+              <p class="ml-[25px] text-[12px] text-[#797979] mt-[8px]">1 de dezembro de 2023</p>
+            </div>
+          </div>
+          <div class="w-[260px] h-[224px] rounded-lg bg-black flex flex-col">
+            <img class="rounded-t-lg" src="/foto-2.png" alt="foto">
+            <h1 class="w-[191px] h-[40px] not-italic font-medium text-[13px] text-white mt-[4px] ml-[12px]">Nova atualização de GTA Online chegando em dezembro</h1>
+            <div class="flex">
+              <p class="text-[#797979] text-[12px] mt-[8px] ml-[12px]">GTA Online</p>
+              <p class="ml-[25px] text-[12px] text-[#797979] mt-[8px]">30 de novembro de 2023</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="w-full h-[630px] bg-[#101010] flex justify-center">
+    <div class="bg-[url('/fundo.png')] w-[1160px] h-[535px] flex items-center">
+      <div class="w-full h-full">
+        <img class="ml-[76px] mt-[77px]" src="/Rock-Games-Launcher.png" alt="Rock Games Launcher">  
+        <h1 class="ml-[76px] mt-[32px] w-[326px] h-[96px] not-italic font-medium text-[32px]">
+          Todos os seus jogos em um só lugar.
+        </h1>
+        <button class="ml-[74px] mt-[64px] w-[260px] h-[63px] not-italic font-medium text-[20px] bg-[#FCAF17] rounded-lg">Download</button>
+      </div>
+      <img class="h-[471px] w-auto" src="/Games-Launcher.png" alt="Games Launcher">
+    </div>
+  </div>
+  <div class="w-[full] h-[238px] bg-black flex items-center">
+    <a href="#"><img class="ml-[140px]" src="/Logo-1.png" alt="Logo"></a>
+    <div class="h-[52px] w-[355px] ml-[40px]">
+      <div class="w-full h-1/2">
+        <a href="#" class="not-italic font-medium text-[16px] text-[#B1B1B1]">CONTATO</a>
+        <a href="#" class="not-italic font-medium text-[16px] text-[#B1B1B1] ml-[24px]">SOBRE</a>
+        <a href="#" class="not-italic font-medium text-[16px] text-[#B1B1B1] ml-[24px]">SUPORTE</a>
+        <a href="#" class="not-italic font-medium text-[16px] text-[#B1B1B1] ml-[24px]">EMPREGOS</a>
+      </div>
+      <p class="tracking-[0.011em] text-[#797979] not-italic font-light text-[13px]">©2023 Rockstar Games, Inc. - YWR Studio.  </p>
+    </div>
+    <div class="w-[248px] h-[24px] flex ml-auto mr-[140px]">
+      <a href="#">
+        <img src="/twitch.png" alt="twitch">
+      </a>
+      <a href="#">
+        <img class="ml-[32px]" src="/instagram.png" alt="instagram">
+      </a>
+      <a href="#">
+        <img class="ml-[32px]" src="/twitter.png" alt="twitter">
+      </a>
+      <a href="#">
+        <img class="ml-[32px]" src="/youtube.png" alt="youtube">
+      </a>
+      <a href="#">
+        <img class="ml-[32px]" src="/facebook.png" alt="facebook">
+      </a>
+    </div>
+  </div>
+</template>
